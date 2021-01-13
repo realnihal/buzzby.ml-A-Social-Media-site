56fa9cfd6a1cc130df466c19f68d09cea9511db3
Nihal Puram <puramnihal@gmail.com>
2021-01-13 13:52:28 +0530

    main page added

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -5,8 +5,16 @@ main = Blueprint('main', __name__)
 
 
 @main.route("/")
+def main():
+    return render_template('main.html')
+
+
+
+
 @main.route("/home")
 def home():
+    if not current_user.is_authenticated:
+        return redirect(url_for('main.main'))
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
