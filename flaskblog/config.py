7ca3ac9ac19e428bccfbdc8a48e96f1fef6bf40b
Nihal Puram <puramnihal@gmail.com>
2021-01-13 12:06:27 +0530

    changed database

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Nihalpuram007@localhost/flaskblog'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
