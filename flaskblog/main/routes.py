d4cb6a0c5f10098711b6ab39a44f2fed53146dce
Nihal Puram <puramnihal@gmail.com>
2021-01-13 22:49:58 +0530

    news14

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, Blueprint
 from flaskblog.models import Post, User
 from flask_login import current_user
-from flaskblog.news import create_news
+from flaskblog.news import create_news, create_news_business
 
 
 main = Blueprint('main', __name__)
@@ -27,8 +27,19 @@ def about():
 
 
 @main.route("/news")
-def news():
+def news_business():
     article = create_news()
+    author = article[0]
+    titles = article[1]
+    urlsOfNews = article[2]
+    descriptions = article[3]
+    num = len(titles)
+    return render_template('news.html', title='News', author=author,titles=titles,urls=urlsOfNews,descriptions=descriptions,length=num)
+
+
+@main.route("/news/business")
+def news():
+    article = create_news_business()
     author = article[0]
     titles = article[1]
     urlsOfNews = article[2]
--- flaskblog/news.py
+++ flaskblog/news.py
@@ -4,7 +4,26 @@ import json
 
 
 def create_news():
-    url="http://newsapi.org/v2/top-headlines?country=in&sports&apiKey=7b57dbfa104847e6b72e2a4b28e51401"
+    url="http://newsapi.org/v2/top-headlines?country=in&apiKey=7b57dbfa104847e6b72e2a4b28e51401"
+
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+
+    articles=data["articles"]
+    titles=[]
+    urlsOfNews=[]
+    author =  []
+    descriptions=[]
+    for i in articles:
+        author.append(i['author'])
+        titles.append(i["title"])
+        urlsOfNews.append(i["url"])
+        descriptions.append(i["description"])
+    return [author, titles, urlsOfNews, descriptions]
+
+
+def create_news_business():
+    url="http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=7b57dbfa104847e6b72e2a4b28e51401"
 
     response = urllib.urlopen(url)
     data = json.loads(response.read())
