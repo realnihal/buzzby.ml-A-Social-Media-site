76ffc3fa5d15016a56854557ecf3be839afa55ca
Nihal Puram <puramnihal@gmail.com>
2021-01-15 22:46:25 +0530

    links changed

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -9,15 +9,19 @@ main = Blueprint('main', __name__)
 
 @main.route("/")
 def start():
-    return render_template('main.html')
+    if current_user.is_authenticated:
+        return home()
+    else:
+        return render_template('main.html')
 
 @main.route("/home")
 def home():
     if not current_user.is_authenticated:
         return start()
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    else:
+        page = request.args.get('page', 1, type=int)
+        posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+        return render_template('home.html', posts=posts)
 
 
 @main.route("/about")
