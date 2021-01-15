959b0239066bcfb9f1267d8948c89052a013f86a
Nihal Puram <puramnihal@gmail.com>
2021-01-15 22:38:08 +0530

    blank replaced

---
+++ weather.py
@@ -0,0 +1,32 @@
+# importing requests and json
+import requests, json
+# base URL
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+CITY = "Hyderabad"
+API_KEY = "cd0c3ecb0b73431373fe3c3a36caffb1"
+# upadting the URL
+URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
+# HTTP request
+response = requests.get(URL)
+# checking the status code of the request
+if response.status_code == 200:
+   # getting data in the json format
+   data = response.json()
+   # getting the main dict block
+   main = data['main']
+   # getting temperature
+   temperature = main['temp']
+   # getting the humidity
+   humidity = main['humidity']
+   # getting the pressure
+   pressure = main['pressure']
+   # weather report
+   report = data['weather']
+   print(f"{CITY + " weather"}")
+   print(f"Temperature: {temperature}")
+   print(f"Humidity: {humidity}")
+   print(f"Pressure: {pressure}")
+   print(f"Weather Report: {report[0]['description']}")
+else:
+   # showing the error message
+   print("Error in the HTTP request")
