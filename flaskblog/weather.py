6392d44493f41f23aef42ffd24d113cbbe17a57a
Nihal Puram <puramnihal@gmail.com>
2021-01-16 22:07:57 +0530

    weather3

--- flaskblog/weather.py
+++ flaskblog/weather.py
@@ -1,21 +1,21 @@
 import requests, json
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-CITY = "Hyderabad"
 API_KEY = "cd0c3ecb0b73431373fe3c3a36caffb1"
 
 URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
-response = requests.get(URL)
-if response.status_code == 200:
-   data = response.json()
-   main = data['main']
-   temperature = main['temp']
-   humidity = main['humidity']
-   pressure = main['pressure']
-   report = data['weather']
-   print(f"{CITY:-^30}")
-   print(f"Temperature: {temperature}")
-   print(f"Humidity: {humidity}")
-   print(f"Pressure: {pressure}")
-   print(f"Weather Report: {report[0]['description']}")
-else:
-   print("Error in the HTTP request")
+
+
+
+def weather_data(CITY):
+    city=CITY
+    response = requests.get(URL)
+    if response.status_code == 200:\
+        data = response.json()
+        main = data['main']
+        temperature = main['temp']
+        humidity = main['humidity']
+        pressure = main['pressure']
+        report = data['weather']
+        return [CITY,temperature,humidity,pressure,report]
+    else:
+    print("Error in the HTTP request")
