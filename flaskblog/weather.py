066c9f00ab22c9a0d5e4d5f3e6492d8adcd058b2
Nihal Puram <puramnihal@gmail.com>
2021-01-17 09:37:29 +0530

    weather page

--- flaskblog/weather.py
+++ flaskblog/weather.py
@@ -2,9 +2,9 @@ import requests, json
 
 
 def weather_data(CITY):
-    URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
     BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
     API_KEY = "cd0c3ecb0b73431373fe3c3a36caffb1"
+    URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
     response = requests.get(URL)
     if response.status_code == 200:
         data = response.json()
